e7ac7c389e65ce40732881170c7c287e92d2aa21
James Nahajski <james.nahajski@sky.uk>
2022-02-18 12:24:45 +0000

    Working gradle version :)

---
+++ src/main/java/james/springboot/spring_game/Controllers/TestController.java
@@ -0,0 +1,12 @@
+package james.springboot.spring_game.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String test() {
+        return "asd\n";
+    }
+}
